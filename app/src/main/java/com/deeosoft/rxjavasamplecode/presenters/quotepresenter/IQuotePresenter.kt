739a420214f3491a0a3cea7c1613ede6b895398a
mac <user@macs-MacBook-Pro.local>
2021-06-30 15:01:04 +0100

    add quote ui layout

--- app/src/main/java/com/deeosoft/rxjavasamplecode/presenters/quotepresenter/IQuotePresenter.kt
+++ app/src/main/java/com/deeosoft/rxjavasamplecode/presenters/quotepresenter/IQuotePresenter.kt
@@ -1,4 +1,19 @@
 package com.deeosoft.rxjavasamplecode.presenters.quotepresenter
 
+import androidx.lifecycle.LiveData
+import com.deeosoft.rxjavasamplecode.domain.entities.Quote
+
 interface IQuotePresenter {
+
+    fun insert(quote: Quote)
+
+    fun update(quote: Quote)
+
+    fun delete(quote: Quote)
+
+    fun deleteAllQuotes()
+
+    fun getAllQuotes(): LiveData<List<Quote>>
+
+    fun loadQuotes()
 }
