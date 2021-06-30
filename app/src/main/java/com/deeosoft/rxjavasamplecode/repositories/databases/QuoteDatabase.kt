be336f63edb73cbb164165319af26f1799dc1dce
mac <user@macs-MacBook-Pro.local>
2021-06-30 12:13:17 +0100

    dao, database and repository added

--- app/src/main/java/com/deeosoft/rxjavasamplecode/repositories/databases/QuoteDatabase.kt
+++ app/src/main/java/com/deeosoft/rxjavasamplecode/repositories/databases/QuoteDatabase.kt
@@ -1,2 +1,11 @@
 package com.deeosoft.rxjavasamplecode.repositories.databases
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.deeosoft.rxjavasamplecode.domain.entities.Quote
+import com.deeosoft.rxjavasamplecode.repositories.doas.QuoteDao
+
+@Database(entities = [Quote::class], version = 1)
+abstract class AppDatabase: RoomDatabase(){
+    abstract fun quoteDao(): QuoteDao
+}
--- app/src/main/java/com/deeosoft/rxjavasamplecode/repositories/quoterepository/QuoteRepository.kt
+++ app/src/main/java/com/deeosoft/rxjavasamplecode/repositories/quoterepository/QuoteRepository.kt
@@ -1,4 +1,54 @@
 package com.deeosoft.rxjavasamplecode.repositories.quoterepository
 
-class QuoteRepository {
+import android.util.Log
+import com.deeosoft.rxjavasamplecode.domain.entities.Quote
+import com.deeosoft.rxjavasamplecode.repositories.doas.QuoteDao
+import io.reactivex.Completable
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+class QuoteRepository(private val quoteDao: QuoteDao): IQuoteRepository {
+
+    override fun insert(quote: Quote) {
+        quoteDao.insert(quote)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                { Log.d("RXJAVA", "insert successful")},
+                { Log.d("RXJAVA", "insert error")}
+            )
+    }
+
+    override fun update(quote: Quote) {
+        quoteDao.update(quote)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {Log.d("RXJAVA", "update successful")},
+                {Log.d("RXJAVA", "update error")}
+            )
+    }
+
+    override fun delete(quote: Quote) {
+        quoteDao.delete(quote)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {Log.d("RXJAVA", "delete successful")},
+                {Log.d("RXJAVA", "delete error")}
+            )
+    }
+
+    override fun deleteAllQuotes() {
+        Completable.fromAction{quoteDao.deleteAllQuotes()}
+            .observeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {Log.d("RXJAVA", "Delete all successful")},
+                {Log.d("RXJAVA", "Delete all error")}
+            )
+    }
+
+    override fun getAllQuotes(): Observable<List<Quote>> = quoteDao.getAllQuotes()
 }
